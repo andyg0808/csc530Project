e87a66985fe1baa30a25c7b4b16c232921dd796a
Andrew Gilbert <andrewg800@gmail.com>
2017-05-04 11:30:40 -0700

    Get result values out of solver
    First example is complete.

--- src/main/scala/work/k33/calpoly/csc530/Basic.scala
+++ src/main/scala/work/k33/calpoly/csc530/Basic.scala
@@ -1,86 +1,38 @@
 package work.k33.calpoly.csc530
 
-import java.util.HashMap
-
-import com.microsoft.z3.{ArithExpr, BoolExpr, Context}
+import com.microsoft.z3.{BoolExpr, Context, Expr, IntNum}
 
 object Basic {
-  /*
-  Based on MIT-licensed example from Z3 (https://github.com/Z3Prover/z3)
-
-  Z3 License
-  Copyright (c) Microsoft Corporation
-  All rights reserved.
-  MIT License
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of
-  this software and associated documentation files (the "Software"), to deal in
-  the Software without restriction, including without limitation the rights to
-  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-  of the Software, and to permit persons to whom the Software is furnished to do
-  so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-  */
   def main(args: Array[String]): Unit = {
     first()
   }
 
   def first() {
-    val cfg = new HashMap[String, String]()
-    cfg.put("proof", "true")
-    val ctx = new Context(cfg)
+    val ctx = new Context()
     val x = ctx.mkIntConst("x")
     val y = ctx.mkIntConst("y")
     val assert1 = ctx.mkEq(x, y)
     val assert2 = ctx.mkEq(ctx.mkMul(x, ctx.mkInt(2)), ctx.mkAdd(x, ctx.mkInt(10)))
     val combined = ctx.mkAnd(assert1, assert2)
-    findExample(ctx, combined, false)
+    findExample(ctx, combined, false, Array(x, y))
   }
 
-  def example() {
-    val cfg = new HashMap[String, String]()
-    cfg.put("proof", "true")
-    val ctx = new Context(cfg)
-
-    val U = ctx.mkUninterpretedSort(ctx.mkSymbol("U"))
-    val g = ctx.mkFuncDecl("g", U, U)
-
-    val x = ctx.mkConst("x", U)
-    val y = ctx.mkConst("y", U)
-
-    val gx = g.apply(x)
-    val gy = g.apply(y)
-
-    val eq = ctx.mkEq(x, y)
-    val f = ctx.mkEq(gx, gy)
-
-    findExample(ctx, ctx.mkImplies(eq, f), false)
-  }
-
-  def findExample(ctx: Context, f: BoolExpr, useMBQI: Boolean) {
+  /*
+  Slightly based on example from Z3 (https://github.com/Z3Prover/z3)
+  */
+  def findExample(ctx: Context, f: BoolExpr, useMBQI: Boolean, consts: Array[Expr]) {
     val s = ctx.mkSolver()
-    val p = ctx.mkParams()
-    p.add("mbqi", useMBQI)
-    s.setParameters(p)
-    //s.add(ctx.mkNot(f))
     s.add(f)
     val q = s.check()
-    println("question" + f)
-    println("result: " + q)
-    //println("Proof: " + s.getProof())
     val model = s.getModel
-    println(model)
-    //model.getDecls.foreach(func => println(func.getName + " " + func))
+     for (i <- consts) {
+       val interp = model.getConstInterp(i)
+       if (interp.isIntNum()) {
+         interp match {
+           case intnum: IntNum => println(i + ": " + intnum.getInt)
+         }
+       }
+     }
   }
 }
 
